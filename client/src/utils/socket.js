382aca075828e037a09eaa218c4344e07bccd562
Manoj <manojkumarmondal554@gmail.com>
2022-06-30 14:28:34 +0530

    configuration setted for deployment

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_SERVER_URL;
+const URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_PRODUCTION_SERVER_URL : process.env.NEXT_PUBLIC_SERVER_URL;
 
 const socket = io(URL, { autoConnect: false });
 
